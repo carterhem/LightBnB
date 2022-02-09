fe2ec2e024ad1d6a5c361924f6e6f6c0bace797c
Carter Hemington <carter.hemington@gmail.com>
2022-02-09 21:51:59 +0000

    5 needs work

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,8 +1,8 @@
-SELECT
-FROM
-JOIN
-WHERE
-GROUP
-HAVING
-ORDER
-LIMIT
+SELECT properties.id, title, cost_per_night, start_date, AVG(rating) as average_rating
+FROM reservations
+JOIN properties ON property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1 AND end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
