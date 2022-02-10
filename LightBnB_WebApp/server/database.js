6b8684cfba7f7fe776764a024b8b7ea2d148118e
Carter Hemington <carter.hemington@gmail.com>
2022-02-10 22:17:08 +0000

    addProperty function implemented

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -106,21 +106,6 @@ ORDER BY reservations.start_date
 LIMIT $2
 `;
 
-// return pool.query(`
-//     SELECT thumbnail_photo_url,properties.id,title,start_date, end_date,cost_per_night,number_of_bathrooms,number_of_bedrooms,parking_spaces, avg(rating)::NUMERIC as average_rating
-//     FROM reservations
-//     JOIN properties ON reservations.property_id = properties.id
-//     JOIN property_reviews ON properties.id = property_reviews.property_id 
-//     WHERE reservations.guest_id = $1
-//     AND reservations.end_date < now()::date
-//     GROUP BY properties.id, reservations.id
-//     ORDER BY reservations.start_date
-//     LIMIT $2
-//   `, [guest_id, limit]).then(res => {
-//         console.log(res.rows);
-//         return res.rows;
-//     })
-
 
   return pool
   .query(queryBuild, [guest_id, limit])
@@ -215,9 +200,24 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+ 
+  return pool
+.query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+.then((result) =>  result.rows[0])
+.catch((err) => {
+  console.log('NULL');
+});
+
+
+// return pool
+// .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+// .then((result) =>  result.rows[0])
+// .catch((err) => {
+//   console.log('NULL');
+// });
 }
 exports.addProperty = addProperty;
